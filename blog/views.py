17cd8eeb28fccbafeafaf534c6e05db86e024606
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-02 17:18:13 +0330

    add blog_page function

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog_page(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'blog_page.html', context)
