7060f9827f1b47cf8526e889cffc7ab036eb1b6d
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-02 13:17:27 +0330

    create db create simple table for my apps(blog,ai_lab,portfolio)

--- ai_lab/models.py
+++ ai_lab/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class AICreation(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    creation_type = models.CharField(max_length=100)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
