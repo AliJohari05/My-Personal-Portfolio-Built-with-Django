60862bca24b6d1ccaf5d4674293f6e2af83dae85
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-02 16:40:43 +0330

    add data from db with DTL

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    projects = Project.objects.all()
+    posts = Post.objects.all()
+    ai_creation = AICreation.objects.all()
+    context = {'projects':projects,
+               'posts':posts,
+               'ai_creation':ai_creation}
+    return render(request, 'home.html', context)
