e4b93ed2b8cc918fed97100c3748cbcbe8b69e63
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-05 17:10:43 +0330

    add category class

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from tinymce.models import HTMLField
 
 # Create your models here.
-
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = HTMLField()
