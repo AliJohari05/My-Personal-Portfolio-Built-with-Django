a4ceea2f30ab0b9bc6561379e6b72505f9bc9d10
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-05 17:20:28 +0330

    add some option for admin dashboard

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title','created_on','image','category')
     search_fields = ('title','category','content')
 admin.site.register(Post)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category)
