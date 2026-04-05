0e6939cef3fda48925db9c264bb896393e33712e
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-05 17:35:44 +0330

    send category for use it in html file

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ def blog_page(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
+        'category': category,
         'page_obj': page_obj
     }
     return render(request, 'blog/blog.html', context)
