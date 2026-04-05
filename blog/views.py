47981ce560c0faca14704e6fcebd21aec5c146db
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-05 17:35:27 +0330

    add category in views

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 def blog_page(request):
+    category = get_object_or_404(Category, name='category_name')
     post_list = Post.objects.all().order_by('-created_on')
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page')
