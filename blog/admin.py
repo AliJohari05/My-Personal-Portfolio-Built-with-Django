fc4614da640499ff17fdbc80703e9734b8bb4297
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-05 17:20:19 +0330

    add some option for admin dashboard

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_on','image','category')
+    search_fields = ('title','category','content')
 admin.site.register(Post)
