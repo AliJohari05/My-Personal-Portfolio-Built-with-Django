f25094a2469603c3ce5571ccc05720fadfcd1b1a
AliJohari05 <alitehranijohari1384@gmail.com>
2026-04-06 12:52:17 +0330

    add Comment model

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,16 @@ class Post(models.Model):
     categories = models.ManyToManyField(Category, blank=True)
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80, verbose_name="Your Name")
+    body = models.TextField(verbose_name="Comment")
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_at'] 
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
